bf2a0ac055b584fb8965d2aa0d66915edcd264d2
木口 <jinkaku8@gmail.com>
2024-05-14 22:08:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path'); // pathモジュールを追加
+
 const app = express();
 
 // 静的ファイルの提供
 app.use(express.static('public'));
 
+// ルートの定義
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.htmlを返す
+});
+
 // サーバーの起動
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
