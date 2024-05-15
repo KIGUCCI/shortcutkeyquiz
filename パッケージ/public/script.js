e04d9a4bf677b29e5da611fd7e958ac21003ddf3
KIGUCCI <jinkaku8@gmail.com>
2024-05-15 19:58:49 +0900

    Fix deleted files and add public folder

--- パッケージ/public/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-document.getElementById('learn').addEventListener('click', () => {
-  window.location.href = '学習モード.html';
-});
-document.getElementById('quiz').addEventListener('click', () => {
-  window.location.href = 'クイズモード.html';
-});
-
