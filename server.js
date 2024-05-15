b7784cb1c0e7686e465e02f5399fe8e702a509a2
木口 <jinkaku8@gmail.com>
2024-05-15 19:18:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // 静的ファイルの提供（public フォルダを参照せず、ファイルの実際の場所を指定）
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ルートの定義
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/public', 'index.html')); // index.htmlを返す
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.htmlを返す
 });
 
 // サーバーの起動
