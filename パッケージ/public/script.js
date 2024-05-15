7e43a984d3de307fea684a640c8f4f55a2ee5282
KIGUCCI <jinkaku8@gmail.com>
2024-05-15 19:56:16 +0900

    Add public folder with index.html

---
+++ パッケージ/public/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.getElementById('learn').addEventListener('click', () => {
+  window.location.href = '学習モード.html';
+});
+document.getElementById('quiz').addEventListener('click', () => {
+  window.location.href = 'クイズモード.html';
+});
+
